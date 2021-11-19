52a251fe0cd98237ddf1a04986622223dbd0a512
Karan20-Selenium <KaranKothavale20@gmail.com>
2021-11-19 14:01:12 +0530

    first commit

---
+++ gs12/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+ objects = None
+ name = models.CharField(max_length=50)
+ roll = models.IntegerField()
+ city = models.CharField(max_length=50)
